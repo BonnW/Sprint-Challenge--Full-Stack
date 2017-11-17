1db042cc017f2c8cb8bb291c225741df37976fba
BonnW <pwonghansa@gmail.com>
2017-11-17 15:41:27 -0600

    connected server to database

--- server.js
+++ server.js
@@ -10,3 +10,21 @@ const server = express();
 
 server.use(cors());
 server.use(body-parser.json());
+
+
+
+// connect to the server
+
+mongoose.Promise = global.Promise;
+const connect = mongoose.connect(
+  'mongodb://localhost/users',
+  { useMongoClient: true }
+);
+
+connect.then(() => {
+  const port = 3000;
+  server.listen(port);
+  console.log(`Server Listening on ${port}`);
+}, (err) => {
+  console.log("ERROR: NO CONNECTION TO SERVER")
+});
